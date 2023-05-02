84028fff417547507042c2628a4f06fa4525a89a
johny <jskjw157@gmail.com>
2023-05-03 08:13:16 +0900

    feat:회원가입 user entity 컬럼

--- src/join/user.entity/user.entity.ts
+++ src/join/user.entity/user.entity.ts
@@ -1,1 +1,53 @@
-export class UserEntity {}
+import {
+    Column,
+    Entity,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+  
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+  
+    @Column()
+    name: string;
+  
+    @Column()
+    position: string;
+  
+    @Column()
+    email: string;
+  
+    @Column()
+    password: string;
+  
+    @Column()
+    contact: string;
+  
+    @Column({ nullable: true })
+    img_url: string;
+  
+    @Column()
+    field: string;
+  
+    @Column()
+    tendency: string;
+  
+    @Column({ nullable: true })
+    intro: string;
+  
+    @Column({ nullable: true })
+    skills: string;
+  
+    @Column({ type: 'int' })
+    like: number;
+  
+    @CreateDateColumn()
+    createdAt: Date;
+  
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+  
